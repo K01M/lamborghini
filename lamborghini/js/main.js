2d22e947db7c5a5a565e678febf06c9b233db417
K01M <michail.kasyanov@yandex.ru>
2023-08-26 14:05:06 +0500

    deleted npm swiper and make cdn, added block blog

---
+++ lamborghini/js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.blog__slider', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-right',
+        prevEl: '.swiper-button-left',
+    },
+});
